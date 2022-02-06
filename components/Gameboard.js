9516f134a26c963123977fccb73d284c51a119b3
Jakub Fábik <jfabik@student.umb.sk>
2022-02-06 09:45:12 +0200

    discovered principles of react native object variables and dynamic js variables what effect runtime differences

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -3,9 +3,11 @@ import React, {useState, useEffect, useCallback} from 'react';
 import {Text, View, Pressable} from 'react-native';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
-
 let cubes = [{},{},{},{},{}]; //important for constructor at getDices
-let circles = [{},{},{}];  //important for constructor at getCircles
+let circles = [{},{},{},{}];  //important for constructor at getCircles
+for (let i = 1; i <= 6; i++){
+  circles[i] = {name:'numeric-' + i + '-circle', color: "steelblue", multiplicator: 0, selected: false};
+}
 const NBR_OF_DICES = 5;
 const NBR_OF_THROWS = 3;
 const WINNING_POINTS = 23;
@@ -15,7 +17,6 @@ export default function Gameboard() {
   const [Total, setTotal] = useState(0);
   const [action, setAction] = useState(0);
   const [status, setStatus] = useState('');
-  const [reload, setReload] = useState(0);
 
 
   function throwDices() {   //Showtime
@@ -27,7 +28,6 @@ export default function Gameboard() {
     }
     setNbrOfThrowsLeft(nbrOfThrowsLeft-1);
     setAction(action+1);
-
 
   }
 
@@ -41,30 +41,9 @@ export default function Gameboard() {
       cubes[i].color = "black";
     }
     setAction(action+1);      //no idea why but withou this is not refreshing MaterialComunityIcons //dirty patch
+    console.log(cubes);
   }
 
-  const circlesValue = () => {
-    let counter = [];
-    let i = 1;
-    for (; i <= 6; i++){
-      counter[i] = 0;
-    }
-    i = 0;
-    for (; i < 5; i++){
-      if(cubes[i].lock){
-        counter[cubes[i].value] = counter[cubes[i].value] + 1;
-      }
-    }
-    // needs condition!!! implementation depends on game rules
-    i = 1;
-    for (; i <= 6; i++){
-      circles[i].multiplicator = counter[i];  //write multiplication
-      if(counter[i]){                         //if counter is higher than zero:
-        circles[i].color = "black";           // black it
-      }
-    }
-  }
-  
 
 //-----------
   const cubesVisual = [];
@@ -85,16 +64,51 @@ export default function Gameboard() {
     }
 }
 getDices();
-console.log("reloaded " + reload);
 console.log("action " + action);
+//------------
 
-function setCircles() {
-  for (let i = 1; i <= 6; i++){
-    circles[i] = {name:'numeric-' + i + '-circle', color: "steelblue", multiplicator: 0};
+const circlesValue = () => {
+  let counter = [];
+  let i = 1;
+  for (; i <= 6; i++){
+    counter[i] = 0;
+  }
+  i = 0;
+  for (; i < 5; i++){
+    if(cubes[i].lock){
+      counter[cubes[i].value] = counter[cubes[i].value] + 1;
+    }
+  }
+  // needs condition!!! implementation depends on game rules
+  i = 1;
+  for (; i <= 6; i++){
+      if(circles[i].selected){
+      circles[i].multiplicator = counter[i];  //write multiplication
+      if(counter[i]){                         //if counter is higher than zero:
+        circles[i].color = "black";           // black it
+      }
+    }
+    else{
+      circles[i].multiplicator = 0;
+      circles[i].color = "steelblue";
+    }
   }
 }
-setCircles();
-//------------
+
+
+function lockCircle(i){
+      if(!circles[i].selected){
+        circles[i].selected = true;
+        
+      }
+      else{
+        circles[i].selected = false;
+      }
+
+  //console.log(circles);
+  setAction(action+1);
+}
+
 const circlesVisual = [];
 const getCircles = () =>{
   circlesValue();
@@ -102,12 +116,16 @@ const getCircles = () =>{
     circlesVisual.push(
      <View key={'couple '+i} style={[styles.gameboard,{margin: 5}]}>
       <Text key={'multi '+i}>{circles[i].multiplicator}</Text>
-      <MaterialCommunityIcons
-        name={circles[i].name}
-        key={'circle'+i}
-        size={40}        
-        color={circles[i].color}
-      />
+      <Pressable 
+        key={'cube'+i}
+        onPress={()=> lockCircle(i)}>
+          <MaterialCommunityIcons
+            name={circles[i].name}
+            key={'circle'+i}
+            size={40}        
+            color={circles[i].color}
+          />
+        </Pressable>
       </View> 
     )
   }
@@ -137,6 +155,8 @@ function checkWinner() {
     setStatus('Keep on throwing');
   }
 }
+
+
   return(
     <View style={styles.gameboard}>
       <View style={styles.flex}>{cubesVisual}</View>
@@ -145,7 +165,7 @@ function checkWinner() {
       <Pressable style={styles.button}
         onPress={() => throwDices()}>
           <Text style={styles.buttonText}>Throw dices</Text>
-        </Pressable>
+      </Pressable>
       <Text style={styles.gameinfo}>Total: {Total}</Text>
         <View style={styles.flex}>{circlesVisual}</View>
     </View>
